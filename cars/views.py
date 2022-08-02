f87aead48d0dadeb0e6bee88fc8ba8efb6182904
mateusz <gmateusz92@gmail.com>
2022-08-02 22:30:20 +0200

    first

--- cars/views.py
+++ cars/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from cars.models import Car
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def cars(request):
-    return render(request, 'cars/cars.html', {})
+    cars = Car.objects.order_by('created_date')
+    paginator = Paginator(cars, 3)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
+    return render(request, 'cars/cars.html', {'cars': paged_cars })
 
-def car_detail(request, id):
+def car_detail(request, id): #wyswietla konkretny wybrany samochod
     single_car = get_object_or_404(Car, pk=id)
-
 
     return render(request, 'cars/car_detail.html', {'single_car': single_car})
