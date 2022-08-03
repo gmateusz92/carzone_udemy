b658a02f676713ce96253931353d987f24dfd7b2
mateusz <matek1600@o2.pl>
2022-08-03 15:33:01 +0200

    first

--- cars/views.py
+++ cars/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from cars.models import Car
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 
 def cars(request):
     cars = Car.objects.order_by('created_date')
-    paginator = Paginator(cars, 3)
+    paginator = Paginator(cars, 4)
     page = request.GET.get('page')
     paged_cars = paginator.get_page(page)
     return render(request, 'cars/cars.html', {'cars': paged_cars })
@@ -13,3 +14,15 @@ def car_detail(request, id): #wyswietla konkretny wybrany samochod
     single_car = get_object_or_404(Car, pk=id)
 
     return render(request, 'cars/car_detail.html', {'single_car': single_car})
+
+def search(request):
+    cars = Car.objects.order_by('-created_date')  # latest cars na stronie
+
+    if 'keyword' in request.GET: #jezeli wpiszemy w search jakis keyword
+        keyword = request.GET['keyword']# przechowujemy keyword w tej zmiennej keyword
+        if keyword: #sprawdzam czy keyword nie jest puste
+            cars = cars.filter(Q(description__icontains=keyword) | Q(car_title__icontains=keyword)) #icointains szuka wszedzie takie klucza liter
+
+        return render(request, 'cars/search.html', {'cars': cars, })
+
+
--- pages/views.py
+++ pages/views.py
@@ -7,7 +7,23 @@ def home(request):
     teams = Team.objects.all()
     featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True) #featrued cars na stronie
     all_cars = Car.objects.order_by('-created_date') #latest cars na stronie
-    return render(request, 'pages/home.html', {'teams': teams, 'featured_cars': featured_cars, 'all_cars': all_cars})
+    #search_fields = Car.objects.values('model', 'city', 'year', 'body_style') # pobiera wartosci z klacy Car do wyszukiwarki na stronie home
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct() #value list flat=True zwraca liste a nie slownik
+    year_search = Car.objects.values_list('year', flat=True).distinct() # distinct() zapobiega pobieraniu takich samych wierszy z bazy danych
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+
+    data = {  'model_search': model_search,
+              'city_search': city_search,
+              'year_search': year_search,
+              'body_style_search': body_style_search,
+              'teams': teams,
+              'featured_cars': featured_cars,
+              'all_cars': all_cars,
+              }
+
+    return render(request, 'pages/home.html', data)
+
 
 def about(request):
     teams = Team.objects.all()
